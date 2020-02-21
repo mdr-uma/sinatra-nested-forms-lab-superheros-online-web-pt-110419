0ddbbbbb03a328008d23c987716d562b7cf50e77
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-21 04:43:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team = Team.new(params[:team])
-      binding.pry
+      @team = Team.new(params[:team], params[:motto])
+      # binding.pry
       erb :team
     end
 
